517c24bd785be918d2253d0804950b48957b2512
gullpacco <gullpacco@gmail.com>
2016-05-04 08:43:10 +0200

    Laser aggiornati

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -11,14 +11,22 @@ public abstract class Laser : MonoBehaviour {
     protected float distanceToReach,
                     currentEnd;
     protected bool distChanged = true;
+    public Material LasMaterial;
+    bool hasStarted;
 
 
     protected virtual void Awake()
     {
+         gameObject.AddComponent<LineRenderer>() ;
         las = GetComponent<LineRenderer>();
+        las.material = LasMaterial;
         particle = transform.GetChild(0).gameObject;
         las.SetPosition(0, transform.position);
-        
+        Debug.Log(transform.position);
+        las.SetPosition(1, transform.position);
+        las.SetWidth(0.2f, .2f);
+
+        Invoke("Uneh", 0.6f);
 
     }
 
@@ -26,22 +34,47 @@ public abstract class Laser : MonoBehaviour {
     void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	protected virtual void Update () {
-        LaserLength();
 
-        if (distChanged)
-            ExtendBeam();
-        else
-            FinalDist();
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (!hasStarted)
+        {
+            for (int i = 0; i < 20; i++) { 
+            LaserLength();
 
-        las.SetPosition(0, transform.position);
-        las.SetPosition(1, distance);
-        particle.transform.position = distance;
+            if (distChanged)
+                ExtendBeam();
+            else
+                FinalDist();
 
+            las.SetPosition(0, transform.position);
+            //if(hasStarted)
+            las.SetPosition(1, distance);
+
+            particle.transform.position = distance;
+        }
+
+        }
+
+        else { LaserLength();
+
+            if (distChanged)
+                ExtendBeam();
+            else
+                FinalDist();
+
+            las.SetPosition(0, transform.position);
+            //if(hasStarted)
+            las.SetPosition(1, distance);
+
+            particle.transform.position = distance;}
     }
 
+    void Uneh()
+    {
+        hasStarted = true;
+    }
 
     protected abstract void LaserLength();
     protected abstract void ExtendBeam();
