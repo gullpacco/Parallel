9a0c6c687085666600877b17e973a1c36e13931f
Federico Giorsetti <giorsetti.federico@gmail.com>
2016-07-13 17:03:18 +0200

    Animazioni nuova selezione livelli

--- Assets/Scripts/Menu2.cs
+++ Assets/Scripts/Menu2.cs
@@ -3,32 +3,79 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Menu2 : MonoBehaviour {
-
+	
+	public Button btnStart;
+	public Button btnOptions;
+	public Button btnCredits;
+	public Button btnQuit;
+	
 	// Use this for initialization
-	void Start () {
+	/*void Start () {
 		Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		/*if(Input.GetKeyDown(KeyCode.Space))
+		if(Input.GetKeyDown(KeyCode.Space))
 		{
 			Cursor.visible = true;
-		}*/
+		}
+	}*/
+	
+    void Start() {
+       
+    }
+    void Update() {
+		
+    }
+	
+	
+	
+	public void EnableAnimator(Animator anim)
+	{
+		anim.SetBool("isDisplayed", true);
+	}
+	
+	public void EnableFirstLeft(Animator anim)
+	{
+		anim.SetBool("isDisplayed", false);
+		anim.SetBool("isWorld1Displayed", true);
+	}
+	
+	public void EnableSecondLeft(Animator anim)
+	{
+		anim.SetBool("isWorld1Displayed", false);
+		anim.SetBool("isWorld2Displayed", true);
+	}
 
-
+	public void DisableMenuAnimator(Animator anim)
+	{
+		anim.SetBool("isMenuDisplayed", false);
+		btnStart.interactable=true;
+		btnOptions.interactable=true;
+		btnCredits.interactable=true;
+		btnQuit.interactable=true;
+	}
+	
+	public void EnableMenuAnimator(Animator anim)
+	{
+		anim.SetBool("isMenuDisplayed", true);
+		btnStart.interactable=false;
+		btnOptions.interactable=false;
+		btnCredits.interactable=false;
+		btnQuit.interactable=false;
+	}
+	
+	public void NavigateTo(int scene)
+	{
+		Application.LoadLevel(scene);
 	}
 
 	public void ClickBtn(Button selectedButton)
 	{
 		selectedButton.transform.localScale = new Vector2(.8f, .8f); //Il pulsante si rimpicciolisce
-
-		if (selectedButton.name == "btnStart") 
-		{
-			Application.LoadLevel ("LevelSelection");
-		} 
-		else if (selectedButton.name == "btnOptions") 
+		if (selectedButton.name == "btnOptions") 
 		{
 			Application.LoadLevel ("OptionsMenu");
 		} 
