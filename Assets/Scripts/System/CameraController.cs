44055d8657c6b30b14ac64804a3336e82a8f5bde
diegogurr <diegogurrieri@live.com>
2016-09-30 19:48:07 +0200

    Livello con grafica

--- Assets/Scripts/System/CameraController.cs
+++ Assets/Scripts/System/CameraController.cs
@@ -5,6 +5,8 @@ public class CameraController : MonoBehaviour {
 
     private Transform [] playerToFollow;
     private int cursor = 0;
+    public float offset, limit, increment;
+
 
 	// Use this for initialization
 	void Start () {
@@ -22,8 +24,12 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //CameraCheck();
-        transform.position = new Vector3((playerToFollow[0].position.x + playerToFollow[1].position.x)/2, transform.position.y, transform.position.z);
+        transform.position = new Vector3((playerToFollow[0].position.x + playerToFollow[1].position.x)/2 -offset, transform.position.y, transform.position.z);
+        if(offset>limit)
+        offset += increment;
 	}
+
+
 
     //void CameraCheck()
     //{
