7b780ccdae8e0387a6014ba4239eae05d297f433
gullpacco <gullpacco@gmail.com>
2016-10-20 20:36:46 +0200

    PillarLock

---
+++ Assets/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MenuController : MonoBehaviour {
+	public GameObject menuPanel;
+	public GameObject exitPanel;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame()
+	{
+		//Application.loadedLevel (0);
+	}
+
+	public void loadLevel ()
+	{
+		Application.LoadLevel(1);
+	}
+	
+	public void QuitGame()
+	{
+		//menuPanel.GetComponent<TweenAlpha>.
+		menuPanel.SetActive (false);
+		exitPanel.SetActive (true);
+
+	}
+}
