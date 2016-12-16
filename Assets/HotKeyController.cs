66dc8f738449d68422d86bda411b4654d09566b5
LazyGNius <lazygnius@gmail.com>
2016-12-16 23:58:27 +0100

    Revert "Revert "Levels_1-5 + HotkeyController""
    This reverts commit 5c892a368fabf5e9f8268c7a9ef0b9343e1225b7.

---
+++ Assets/HotKeyController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotKeyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown(KeyCode.Alpha1)){
+			Application.LoadLevel("Level_1");
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha2)){
+			Application.LoadLevel("Level_2");
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha3)){
+			Application.LoadLevel("Level_3");
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha4)){
+			Application.LoadLevel("Level_4");
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha5)){
+			Application.LoadLevel("Level_5");
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha6)){
+			Application.LoadLevel("Level_6");
+		}
+	}
+}
